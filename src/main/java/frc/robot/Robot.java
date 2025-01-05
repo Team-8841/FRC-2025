492dddf32ae10b03152aaf8c5d6e7f654c3fb6c3
Austin Langley <adlangley12@gmail.com>
2025-01-04 20:52:15 -0500

    Added tuning variables class

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -15,6 +15,7 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
  */
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
+  Tuning tuneVars = new Tuning(); // Values used for tuning robot functionality
 
   private final RobotContainer m_robotContainer;
 
@@ -26,6 +27,7 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+    tuneVars.initValues();
   }
 
   /**
@@ -42,6 +44,7 @@ public class Robot extends TimedRobot {
     // and running subsystem periodic() methods.  This must be called from the robot's periodic
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
+    tuneVars.updateValues();
   }
 
   /** This function is called once each time the robot enters Disabled mode. */
