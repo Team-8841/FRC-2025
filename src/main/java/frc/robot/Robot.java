be78d584eb2fa8a54d7365507b66f35853e896be
Austin Langley <adlangley12@gmail.com>
2025-03-09 12:04:35 -0400

    Removed Unused Tuning Class

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -18,7 +18,6 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
  */
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
-  Tuning tuneVars = new Tuning(); // Values used for tuning robot functionality
 
   private final RobotContainer m_robotContainer;
 
@@ -32,7 +31,6 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
-    tuneVars.initValues();
 
     // Create a timer to disable motor brake a few seconds after disable.  This will let the robot stop
     // immediately when disabled, but then also let it be pushed more 
@@ -58,7 +56,6 @@ public class Robot extends TimedRobot {
     // and running subsystem periodic() methods.  This must be called from the robot's periodic
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
-    //tuneVars.updateValues();
   }
 
   /** This function is called once each time the robot enters Disabled mode. */
