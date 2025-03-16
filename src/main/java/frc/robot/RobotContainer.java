e9ede89ddbc860254c4c0ad99a3c4879bad87da4
Austin Langley <adlangley12@gmail.com>
2025-03-16 14:37:15 -0400

    English Spelling is hard

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -161,7 +161,7 @@ public class RobotContainer {
     // Vision Reef Auto Alignment
     NamedCommands.registerCommand("AutoAlignReefLeft", new MoveToApril(m_Vision, drivebase, false));
     NamedCommands.registerCommand("AutoAlignReefRight", new MoveToApril(m_Vision, drivebase, true));
-    NamedCommands.registerCommand("AutoALightReefCenter", new MoveToCenterApril(m_Vision, drivebase));
+    NamedCommands.registerCommand("AutoAlignReefCenter", new MoveToCenterApril(m_Vision, drivebase));
     
     m_autoChooser = AutoBuilder.buildAutoChooser();
     SmartDashboard.putData(m_autoChooser);
